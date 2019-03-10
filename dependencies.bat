8971360ee9275aa17b4863d7512aa9c296507503
EnderDev <xenderpugx@gmail.com>
2019-03-10 19:47:11 +0000

    ❌ Removed dependencies.bat

--- dependencies.bat
+++
@@ -1,9 +0,0 @@
-npm install https://github.com/woor/discord.io/tarball/gateway_v6
-npm install ffmpeg-binaries
-npm install opusscript
-npm install node-opus
-npm install ffmpeg
-npm install node-fetch
-npm install systeminformation --save
-npm install os-utils
-pause
